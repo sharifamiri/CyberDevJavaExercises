7ad71fcb05512831d5ab7ad08d0a2e658c8db79f
Sharif Amiri <sharif.amiri4@gmail.com>
2021-10-08 08:27:45 -0700

    car dealership

---
+++ src/day11/CarDealership.java
@@ -0,0 +1,34 @@
+package day11;
+
+import java.util.Scanner;
+
+public class CarDealership {
+
+    public static void main(String[] args) {
+
+        /*
+        car dealer app
+        ask; age, salary, type of car
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int priceOfCar = 20000;
+        int age = scan.nextInt();
+        if(age >= 18){
+            System.out.println("You are ELIGIBLE for a car");
+            System.out.println("How much do you have cash$$$$?");
+            int cash = scan.nextInt();
+            if(cash >= priceOfCar){
+                System.out.println("You have enough to buy a car");
+            }else{
+                System.out.println("You need " + (priceOfCar - cash) + " dollars");
+            }
+        }else{
+            System.out.println("You are NOT ELIGIBLE for a car");
+        }
+
+//        System.out.println(eligibility);
+
+    }
+}
