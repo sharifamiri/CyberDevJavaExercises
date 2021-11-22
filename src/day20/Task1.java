f339c497963499ead0b05a4386a4a99e385c3103
Sharif Amiri <sharif.amiri4@gmail.com>
2021-11-22 06:54:13 -0800

    for loop

--- src/day20/Task1.java
+++ src/day20/Task1.java
@@ -1,5 +1,7 @@
 package day20;
 
+import java.util.Scanner;
+
 public class Task1 {
 
 	public static void main(String[] args) {
@@ -17,6 +19,34 @@ public class Task1 {
 		
 		System.out.println();
 		System.out.println("END");
+
+
+		Scanner sn = new Scanner(System.in);
+		System.out.println("Enter word: ");
+		String word = sn.next();
+		System.out.println(word.substring(2));
+		System.out.println(word.substring(word.length()/2));
+
+//		for(int i=4; i < word.length(); i++){
+//			String name1 = word.substring(2);
+//			System.out.println(name1);
+//		}
+		for(int i = 0; i < word.length()/2; i++){
+			System.out.print(word.charAt(i));
+		}
+		System.out.println();
+		//reverse
+		String str = "";
+		for(int i = word.length()-1; i >=0; i--){
+			word += word.charAt(i);
+		}
+		System.out.println();
+		for(int i = word.length()/2; i < word.length(); i++){
+			System.out.print(word.charAt(i));
+		}
+
+		System.out.println();
+		System.out.println(word);
 		
 	}
 
